9fec3c88050fafe953e510ce96a61332d0d2a465
asdfbat <jonas.s.lunde@outlook.com>
2022-06-09 10:28:28 +0200

    Nicer printout, and now checks files in presto and d22 tape as well

--- scripts/level1_storage/print_compressed.sh
+++ scripts/level1_storage/print_compressed.sh
@@ -2,13 +2,17 @@
 # and checks if there exist compressed versions (comp_comap-*.hd5) in the same directory, and
 # if uncompressed files can be found on tape, at /mn/stornext/d16/cmbco/archive/comap/data/pathfinder/ovro.
 
-echo "__Number of uncompressed and compressed files in all subdirs__"
-echo "dir  uncomp  comp  tape"
+fmt="%-10s%-10s%-10s%-10s%-10s%-10s\n"
+
+printf "__Number of uncompressed and compressed files in all subdirs__\n"
+printf "$fmt" "month" "presto" "uncomp" "comp" "d16tape" "d22tape"
 for d in /mn/stornext/d22/cmbco/comap/protodir/level1/20*/ ; do
     month=${d:45:7}
-
+    
+    presto=$(ssh comap_analysis@presto.caltech.edu ls /comapdata*/pathfinder/level1/$month*/comap-*.hd5 2> /dev/null | wc -l)
     uncomp=$(ls $d\comap-*.hd5 2> /dev/null | wc -l)
     comp=$(ls $d\comp*.hd5 2> /dev/null | wc -l)
-    tape=$(ls /mn/stornext/d16/cmbco/archive/comap/data/pathfinder/ovro/$month/comap-*.hd5 2> /dev/null | wc -l)
-    echo "$month $uncomp $comp $tape"
+    d16tape=$(ls /mn/stornext/d16/cmbco/archive/comap/data/pathfinder/ovro/$month/comap-*.hd5 2> /dev/null | wc -l)
+    d22tape=$(ls /mn/stornext/d22/cmbco_archive/comap/protodir/level1/$month/comap-*.hd5 2> /dev/null | wc -l)
+    printf "$fmt" "$month" "$presto" "$uncomp" "$comp" "$d16tape" "$d22tape"
 done
