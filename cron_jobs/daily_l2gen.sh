7d85be3e30b0392b16edf2b5636aa4dde31e3b63
jgslunde <jonas.s.lunde@outlook.com>
2023-08-25 10:52:12 +0200

    Added scan-detect and l2gen to nightly cron runs

---
+++ cron_jobs/daily_l2gen.sh
@@ -0,0 +1,1 @@
+OMP_NUM_THREADS=2 OPENBLAS_NUM_THREADS=2 MKL_NUM_THREADS=2 mpirun -n 12 python3 -u -m mpi4py /mn/stornext/d22/cmbco/comap/protodir/pipeline/l2gen.py -p /mn/stornext/d22/cmbco/comap/protodir/params/param_allco_default.txt
---
+++ cron_jobs/daily_scandetect.sh
@@ -0,0 +1,2 @@
+python3 /mn/stornext/d22/cmbco/comap/protodir/pipeline/scan_detect/scan_detect.py --runlist /mn/storenext/d22/cmbco/comap/protodir/runlists/runlist_allco.txt --fields co2 co6 co7
+python3 /mn/stornext/d22/cmbco/comap/protodir/pipeline/scan_detect/scan_detect.py --runlist /mn/storenext/d22/cmbco/comap/protodir/runlists/runlist_everything.txt --fields all
