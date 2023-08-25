10837c93ad9a4b73c07c8033101a9b87904f501c
jgslunde <jonas.s.lunde@outlook.com>
2023-08-25 10:09:36 +0200

    no idea what has changed

--- scan_detect/scan_detect.py
+++ scan_detect/scan_detect.py
@@ -15,7 +15,7 @@ featname = ["f0", "optical_pointing", "radio_pointing", "ground_scan",
             "circular_scan", "constant_elevation_scan", "ambient_load",
             "stationary", "sky_dip", "raster"]
 
-blacklist = np.load("/mn/stornext/d22/cmbco/comap/protodir/auxiliary/blacklists/blacklist_observerlog.npy")
+# blacklist = np.load("/mn/stornext/d22/cmbco/comap/protodir/auxiliary/blacklists/blacklist_observerlog.npy")
 
 class MetaData:
     # class containing all interesting info about a file.
@@ -124,10 +124,10 @@ def find_scan_ranges(mjd, status, scan_mode, feat, feat_mjd, field, params):
             print('Unknown status, time:', time, ' status:', status[i])
             # sys.exit()
         if (not was_on_scan) and is_on_scan:
-            starts.append(time + params['OFFSET_START'] * dt)
+            starts.append(time)
 
         if was_on_scan and (not is_on_scan):
-            ends.append(time - params['OFFSET_END'] * dt)
+            ends.append(time)
         i += 1
         was_on_scan = is_on_scan
 
@@ -145,9 +145,9 @@ def find_scan_ranges(mjd, status, scan_mode, feat, feat_mjd, field, params):
     n_scans = len(starts)
     for i in range(n_scans):
         index = n_scans - i - 1
-        if (ends[index] - starts[index] < params['MINIMUM_SCAN_LENGTH'] / (24 * 60)):
-            ends.pop(index)
-            starts.pop(index)
+        # if (ends[index] - starts[index] < params['scandetect_minimum_scan_length'] / (24 * 60)):
+        #     ends.pop(index)
+        #     starts.pop(index)
 
     return np.array((starts, ends)).transpose()
 
@@ -207,128 +207,146 @@ def find_file_dict(foldername, params, verb, mem={}, bad=[]):
                         print('Wrong format:', file)
                         obs_id = -1
                         bad.append(file)
-                    is_in_range = params['LATEST_OBSID'] >= obs_id >= params['EARLIEST_OBSID']
-                    is_blacklisted = obs_id in blacklist
-                    if (str(obs_id) in mem.keys()) and is_in_range and not is_blacklisted:
-                        file_dict[str(obs_id)] = mem[str(obs_id)]
-                    elif new_format and is_in_range and not is_blacklisted:
-                        # print('File: '+file)
-                        file_path = file.replace(foldername, '')
-                        if obs_id < 200:
-                            field = att['source']
-                        else:
-                            try:
-                                field = att['source'].decode('utf-8')
-                            except:  # Got an error on the decode. Inserted try-except.  -Jonas
+
+                    if new_format:
+                        # correct_scan_type = False
+                        # scan_mode = "none"
+                        # for scantype_feature in params["scantypes"]:
+                            # if att["features"]
+                        # for power in range(32):
+                        #     if 2**power & att['features'] and 2**power in params["allowed_scantypes"]:
+                        #         correct_scan_type = True
+                        # if not correct_scan_type:
+                        #     pass
+                        # for bit, name in enumerate(featname):
+                        #     if (att['features'] & 1 << bit):
+                        #         scan_mode = featname[bit]
+                        # if params["ces_only"] and scan_mode != "constant_elevation_scan":
+                        #     pass
+                        # else:
+                        is_in_range = params['obsid_stop'] >= obs_id >= params['obsid_start']
+                        # is_blacklisted = obs_id in blacklist
+                        if (str(obs_id) in mem.keys()) and is_in_range: # and not is_blacklisted:
+                            file_dict[str(obs_id)] = mem[str(obs_id)]
+                        elif is_in_range: # and not is_blacklisted:
+                            # print('File: '+file)
+                            file_path = file.replace(foldername, '')
+                            if obs_id < 200:
                                 field = att['source']
-                        # if field == "NCP": ### comment out
-                        #     print('NCP', obs_id)   ####
+                            else:
+                                try:
+                                    field = att['source'].decode('utf-8')
+                                except:  # Got an error on the decode. Inserted try-except.  -Jonas
+                                    field = att['source']
+                            # if field == "NCP": ### comment out
+                            #     print('NCP', obs_id)   ####
 
-                        if field[0:4] == 'co7,':
-                            field = 'co7'
-                        if field[0:4] == 'co4f':
-                            field = 'co4'
-                        if field[0:4] == 'co2f':
-                            field = 'co2'
-                        if field[0:5] == 'TauAf':
-                            field = 'TauA'
-                        if field[0:7] == 'jupiter':
-                            field = 'jupiter'
-                        #if field[0:7] == 'jupiter': field = 'jupiter'
-                        print('File: ' + file, field)
-                        features = att['features']  # 2 #
-                        is_one_feat = True  # testing for multiple features
-                        scan_mode = None
-                        for bit, name in enumerate(featname):
-                            if (features & 1 << bit):
-                                #print("(f%d) %s" % (bit, featname[bit]))
-                                scan_mode = featname[bit]
-                                # These features are treated as separate fields
-                                if scan_mode == "ambient_load":
-                                    field = scan_mode
-                                    break
-                                if scan_mode == "ground_scan":
-                                    field = scan_mode
-                                    break
-                                if scan_mode == "stationary":
-                                    if field == "NCP":
-                                        pass
-                                    elif field == "ncpfixed":
-                                        pass
-                                    else:
+                            if field[0:4] == 'co7,':
+                                field = 'co7'
+                            if field[0:4] == 'co4f':
+                                field = 'co4'
+                            if field[0:4] == 'co2f':
+                                field = 'co2'
+                            if field[0:5] == 'TauAf':
+                                field = 'TauA'
+                            if field[0:7] == 'jupiter':
+                                field = 'jupiter'
+                            #if field[0:7] == 'jupiter': field = 'jupiter'
+                            print('File: ' + file, field)
+                            features = att['features']  # 2 #
+                            is_one_feat = True  # testing for multiple features
+                            scan_mode = None
+                            for bit, name in enumerate(featname):
+                                if (features & 1 << bit):
+                                    #print("(f%d) %s" % (bit, featname[bit]))
+                                    scan_mode = featname[bit]
+                                    # These features are treated as separate fields
+                                    if scan_mode == "ambient_load":
                                         field = scan_mode
-                                    break
-                                if scan_mode == "sky_dip":  # seems like sky dip is associated with other field
-                                    field = scan_mode
-                                    break
-                                scan_mode_bit = bit
-                                # print(is_one_feat)
-                                ########## Test if multiple features ###########
-                                if not is_one_feat:
-                                    print('Warning, multiple features')
-                                    for bit, name in enumerate(featname):
-                                        if (features & 1 << bit):
-                                            print("(f%d) %s" %
-                                                  (bit, featname[bit]))
-                                is_one_feat = False
-                                # break  # We can in principle have multiple features, for now we just choose first
-                        if (features & 1 << 15):
-                            # print('lissajous!!!!!!!!!!')
-                            scan_mode = "lissajous"
-                        if scan_mode is None:
-                            scan_mode = "other"
-                            field = scan_mode
+                                        break
+                                    if scan_mode == "ground_scan":
+                                        field = scan_mode
+                                        break
+                                    if scan_mode == "stationary":
+                                        if field == "NCP":
+                                            pass
+                                        elif field == "ncpfixed":
+                                            pass
+                                        else:
+                                            field = scan_mode
+                                        break
+                                    if scan_mode == "sky_dip":  # seems like sky dip is associated with other field
+                                        field = scan_mode
+                                        break
+                                    scan_mode_bit = bit
+                                    # print(is_one_feat)
+                                    ########## Test if multiple features ###########
+                                    if not is_one_feat:
+                                        print('Warning, multiple features')
+                                        for bit, name in enumerate(featname):
+                                            if (features & 1 << bit):
+                                                print("(f%d) %s" %
+                                                    (bit, featname[bit]))
+                                    is_one_feat = False
+                                    # break  # We can in principle have multiple features, for now we just choose first
+                            if (features & 1 << 15):
+                                # print(features)
+                                # print('lissajous!!!!!!!!!!')
+                                scan_mode = "lissajous"
+                            if scan_mode is None:
+                                scan_mode = "other"
+                                field = scan_mode
 
-                        scan_mode_bit = features
-                        time = fd[u'spectrometer/MJD']  # ['time']
-                        time_range = (time[0], time[-1])
-                        # ['hk/time_track']
-                        t_status = fd[u'hk/antenna0/deTracker/utc']
-                        # ['hk/lissajous_status']
-                        status = fd[u'hk/antenna0/deTracker/lissajous_status']
-                        el = fd[u'pointing/elActual']  # ['point_tel'][0, :, 1]
-                        az = fd[u'pointing/azActual']  # ['point_tel'][0, :, 0]
+                            scan_mode_bit = features
+                            time = fd[u'spectrometer/MJD']  # ['time']
+                            time_range = (time[0], time[-1])
+                            # ['hk/time_track']
+                            t_status = fd[u'hk/antenna0/deTracker/utc']
+                            # ['hk/lissajous_status']
+                            status = fd[u'hk/antenna0/deTracker/lissajous_status']
+                            el = fd[u'pointing/elActual']  # ['point_tel'][0, :, 1]
+                            az = fd[u'pointing/azActual']  # ['point_tel'][0, :, 0]
 
-                        feat_arr = fd[u'/hk/array/frame/features']
-                        t_feat = fd[u'/hk/array/frame/utc']
-                        tsys_ranges = find_tsys_ranges(t_feat, feat_arr)
-                        scan_ranges = find_scan_ranges(
-                            t_status, status, scan_mode,
-                            feat_arr, t_feat, field, params
-                        )
-                        if scan_mode == 'stationary':
-                            try:
-                                scan_ranges[0, 0] = tsys_ranges[0, 1]
-                                scan_ranges[-1, 1] = tsys_ranges[-1, 0]
-                            except:
-                                print('problem with end of tsys', obs_id)
-                        scan_ranges = np.concatenate(
-                            (tsys_ranges, scan_ranges), axis=0)
-                            
-                        scan_ranges = scan_ranges[scan_ranges[:, 0].argsort()]
-                        # print(scan_ranges)
-                        features = np.zeros(len(scan_ranges[:, 0]))
-                        for i, t in enumerate(scan_ranges.mean(1)):
-                            features[i] = find_feat(t, t_feat, feat_arr)
-                            tsys_bit = 13
-                            if (int(features[i]) & 1 << tsys_bit):
-                                features[i] = 8192
-                            # liss_bit = 15
-                            # if (int(features[i]) & 1<<tsys_bit):
-                            #     features[i] = 32768
-                        t_point = fd[u'pointing/MJD']  # ['time_point']
-                        #plt.plot(t_point, el)
-                        # plt.show()
-                        if len(scan_ranges) > 0:
-                            mean_az, mean_el, el_std = find_mean_values(
-                                scan_ranges, t_point, az, el)
-                            metadata = MetaData(
-                                obs_id, field, scan_mode,
-                                scan_mode_bit, time_range,
-                                scan_ranges, features, mean_az,
-                                mean_el, el_std, file_path)
-                            file_dict[str(obs_id)] = metadata
-                            mem[str(obs_id)] = metadata
+                            feat_arr = fd[u'/hk/array/frame/features']
+                            t_feat = fd[u'/hk/array/frame/utc']
+                            tsys_ranges = find_tsys_ranges(t_feat, feat_arr)
+                            scan_ranges = find_scan_ranges(
+                                t_status, status, scan_mode,
+                                feat_arr, t_feat, field, params
+                            )
+                            if scan_mode == 'stationary':
+                                try:
+                                    scan_ranges[0, 0] = tsys_ranges[0, 1]
+                                    scan_ranges[-1, 1] = tsys_ranges[-1, 0]
+                                except:
+                                    print('problem with end of tsys', obs_id)
+                            scan_ranges = np.concatenate(
+                                (tsys_ranges, scan_ranges), axis=0)
+                                
+                            scan_ranges = scan_ranges[scan_ranges[:, 0].argsort()]
+                            # print(scan_ranges)
+                            features = np.zeros(len(scan_ranges[:, 0]))
+                            for i, t in enumerate(scan_ranges.mean(1)):
+                                features[i] = find_feat(t, t_feat, feat_arr)
+                                tsys_bit = 13
+                                if (int(features[i]) & 1 << tsys_bit):
+                                    features[i] = 8192
+                                # liss_bit = 15
+                                # if (int(features[i]) & 1<<tsys_bit):
+                                #     features[i] = 32768
+                            t_point = fd[u'pointing/MJD']  # ['time_point']
+                            #plt.plot(t_point, el)
+                            # plt.show()
+                            if len(scan_ranges) > 0:
+                                mean_az, mean_el, el_std = find_mean_values(
+                                    scan_ranges, t_point, az, el)
+                                metadata = MetaData(
+                                    obs_id, field, scan_mode,
+                                    scan_mode_bit, time_range,
+                                    scan_ranges, features, mean_az,
+                                    mean_el, el_std, file_path)
+                                file_dict[str(obs_id)] = metadata
+                                mem[str(obs_id)] = metadata
             except OSError:
                 if verb:
                     print('\nUnable to read file:')
@@ -374,36 +392,23 @@ except IndexError:
     print('You need to provide param file as command-line argument')
     sys.exit()
 
-params = {}
-with open(param_file) as f:
-    fr = f.readlines()
 
-    fr = [f[:-1] for f in fr]
+sys.path.append("/mn/stornext/d22/cmbco/comap/jonas/pipeline")  # TODO: Not use hard-coded path.
+from l2gen_argparser import parser
+params = parser.parse_args()
+if not params.runlist:
+    raise ValueError("A runlist must be specified in parameter file or terminal.")
+param_file = params.param
+params = vars(params)  # Accept-mod was written with params as a dict, so we just do Namespace -> dict so I don't have to rewrite stuff.
 
-    frs = [f.split(" = ") for f in fr]
 
-    for stuff in frs:
-        try:
-            i, j = stuff
-            params[str(i).strip()] = eval(j)
-        except ValueError:
-            pass
-        except SyntaxError:
-            if j == '.true.':
-                params[str(i).strip()] = True
-            elif j == '.false.':
-                params[str(i).strip()] = False
-            else:
-                pass
-# print(params)
+runlist_name = params['runlist']
+foldername = params['level1_dir']
+require_tsys = True  # params['REQUIRE_TSYS']
+verb = False  # params['VERBOSE_PRINT']
+remove_eng = True  # params['REMOVE_ENGINEERING_RUNS']
 
-runlist_name = params['RUNLIST']
-foldername = params['LEVEL1_DIR']
-require_tsys = params['REQUIRE_TSYS']
-verb = params['VERBOSE_PRINT']
-remove_eng = params['REMOVE_ENGINEERING_RUNS']
-
-aux_data_path = params['AUX_SAVED_DATA']
+aux_data_path = "/mn/stornext/d22/cmbco/comap/protodir/auxiliary/obj/"  # params['AUX_SAVED_DATA']
 memory_file = 'mem'
 bad_file = 'bad'
 
@@ -482,12 +487,14 @@ field_names = ('jupiter', 'venus', 'TauA', 'shela', 'hetdex', 'patch1',
                'ground_scan', 'stationary', 'sky_dip', 'CasA', 'CygA', 'other',
                'NCP', 'ncpfixed')
 
-try:
-    fieldn = sys.argv[2]
-    field_names = (fieldn,)
-    runlist_name = runlist_name[:-4] + '_' + fieldn + runlist_name[-4:]
-except IndexError:
-    pass
+# try:
+#     fieldn = sys.argv[2]
+#     field_names = (fieldn,)
+#     runlist_name = runlist_name[:-4] + '_' + fieldn + runlist_name[-4:]
+# except IndexError:
+#     pass
+
+print("Runlist: ", runlist_name)
 
 
 def get_jackknives(field):
