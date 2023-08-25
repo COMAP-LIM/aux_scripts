4d5833db0b30fc1a74e3ea328ba6c45b69bf3249
jgslunde <jonas.s.lunde@outlook.com>
2023-08-25 10:00:24 +0200

    a few of the nightly cron jobs now execute when the previous is done instead of at specific times. also now passing the date and month as arguments to all jobs, in case it's the next day by the time some script executes.

---
+++ cron_jobs/daily_tsys_database.sh
@@ -0,0 +1,2 @@
+MONTH="$1"
+python3 -W ignore mn/stornext/d22/cmbco/comap/protodir/comap_aux/level1_database/tsys_database_write.py -n 12 -p /mn/stornext/d22/cmbco/comap/protodir/aux_data/level1_database_files -m $(MONTH)
